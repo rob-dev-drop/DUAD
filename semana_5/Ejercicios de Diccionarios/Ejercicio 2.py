b28bc7a94716375198443719db011ef415a4df52
unknown <apytxd2@gmail.com>
2025-05-28 16:53:21 -0600

    testing delete

--- semana_5/Ejercicios de Diccionarios/Ejercicio 2.py
+++
@@ -1,9 +0,0 @@
-lista_feria = ['cebollino', 'chile', 'papas', 'camote']
-lista_cuanto = [1,7,14,20]
-
-compras = {}
-
-for index in range(0,len(lista_feria)):
-    compras[lista_feria[index]] = lista_cuanto[index]
-
-print(compras)
